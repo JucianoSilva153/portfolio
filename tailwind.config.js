b781b01eac67a346d6a342bdede839e7645f7158
Juciano Silva <jucianodasilvasilvaa@gmail.com>
2023-05-04 19:14:27 +0100

    Remodelação com Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+    },
+    colors: {
+      "Fundo": "#312117",
+      "FundoCards": "#1e150f",
+      "CorPrincipal": "#f79d65",
+      "CorTextEnfase": "#ffffff",
+      "CorTexto": "#A6B0B5"
+    }
+  },
+  plugins: [],
+}
+
